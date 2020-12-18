ee8f941753c7c99d023fba815b9f88fcc71bbd36
KisooYoon <yoooon0104@gmail.com>
2020-12-18 12:01:00 +0900

    Revert "Revert "CoolSMS api module update""
    This reverts commit 44318c195124f370405ea96cb9341ccab5081ab9.

---
+++ Spring_SMS/src/main/java/com/min/edu/model/API_Service.java
@@ -0,0 +1,34 @@
+package com.min.edu.model;
+
+import java.util.HashMap;
+
+import org.json.simple.JSONObject;
+import org.springframework.stereotype.Service;
+
+import net.nurigo.java_sdk.api.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
+
+@Service
+public class API_Service {
+
+	public void certifiedPhoneNumber(String phoneNumber, String cerNum) {
+		String api_key = "인증키 입력"; // coolSMS 사이트에서 받은 인증키
+        String api_secret = "비밀키 입력"; // coolSMS 자체에 발급된 비밀키
+        Message coolsms = new Message(api_key, api_secret);
+
+        // 4개의 입력값 필요
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("to", phoneNumber);    // 수신전화번호
+        params.put("from", "coolSMS에서 등록한 발신번호");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨 -> 발신전화 번호는 coolSMS에 등록해줘야됨
+        params.put("type", "SMS");				// type 방식
+        params.put("text", "휴대폰인증 메시지 : 인증번호는" + "["+cerNum+"]" + "입니다.");
+        params.put("app_version", "test app 1.2"); // application name and version
+        try {
+        	JSONObject obj = (JSONObject) coolsms.send(params);
+            System.out.println(obj.toString());
+        } catch (CoolsmsException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getCode());
+        }
+	}
+}
