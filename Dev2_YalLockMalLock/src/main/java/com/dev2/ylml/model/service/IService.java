794d4b29d60109dabd3231adb916ddad96c0dbaa
jeongeun15 <58517997+jeongeun15@users.noreply.github.com>
2021-01-10 23:33:44 +0900

    SJE feat : 배송 시간 및 거리 산출

--- Dev2_YalLockMalLock/src/main/java/com/dev2/ylml/model/service/IService.java
+++ Dev2_YalLockMalLock/src/main/java/com/dev2/ylml/model/service/IService.java
@@ -1,7 +1,6 @@
 package com.dev2.ylml.model.service;
 
 import java.util.List;
-<<<<<<< HEAD
 import java.util.Map;
 
 import com.dev2.ylml.dto.UserDeliveryListDto;
@@ -29,23 +28,6 @@ public interface IService {
 	 * 배송 소요 시간 및 비용 계산
 	 */
 	public Map<String, Integer> selectDeliveryInfo(Map<String, String> stations);
-=======
-
-import com.dev2.ylml.dto.UserDeliveryListDto;
-import com.dev2.ylml.dto.UserStorageListDto;
-
-public interface IService {
-
-	/**
-	 * 보관 정보 조회(사용자)
-	 */
-	public List<UserStorageListDto> selectUserStorageList(String email);
-	
-	/**
-	 * 배송 물량 확인
-	 */
-	public int selectDeliveryGoods(String storageId);
->>>>>>> branch 'SONGJEONGEUN' of https://github.com/devA-2/YeolLockMalLock.git
 	
 	/**
 	 * 배송 정보 조회(사용자)
