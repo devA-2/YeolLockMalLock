5a7a9d19d0f89edf6b64c13e91f8b03557772e9d
jeongeun15 <58517997+jeongeun15@users.noreply.github.com>
2021-01-10 23:31:12 +0900

    SJE feat : 배송 시간 및 거리 산출

--- Dev2_YalLockMalLock/src/main/java/com/dev2/ylml/controller/StorageBoxController.java
+++ Dev2_YalLockMalLock/src/main/java/com/dev2/ylml/controller/StorageBoxController.java
@@ -1,6 +1,5 @@
 package com.dev2.ylml.controller;
 
-<<<<<<< HEAD
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -112,57 +111,6 @@ public class StorageBoxController {
 	@RequestMapping(value = "/checkDeliveryGoods.do", method = RequestMethod.GET)
 	public String checkDeliveryGoods(String storageId) {
 		int cnt = service.selectDeliveryQty(storageId);
-=======
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.dev2.ylml.dto.UserDeliveryListDto;
-import com.dev2.ylml.dto.UserStorageListDto;
-import com.dev2.ylml.model.service.IService;
-
-@Controller
-public class StorageBoxController {
-	
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
-
-	@Autowired
-	private IService service;
-	
-	@RequestMapping(value = "/storageBoxTest.do", method = RequestMethod.GET)
-	public String test() {
-		return "storageBoxTest";
-	}
-	
-	/**
-	 * 보관 정보 조회(사용자)
-	 * @param email
-	 * @param model
-	 * @return
-	 */
-	@RequestMapping(value = "/userStorageList.do", method = RequestMethod.GET)
-	public String userStorageList(String email, Model model) {
-		List<UserStorageListDto> list = service.selectUserStorageList(email);
-		model.addAttribute("list", list);
-		logger.info("userStorageList.do 실행", list);
-		return "userStorageList";
-	}
-	
-	/**
-	 * 
-	 * @param storageId
-	 * @return
-	 */
-	@RequestMapping(value = "/checkDeliveryGoods.do", method = RequestMethod.GET)
-	public String checkDeliveryGoods(String storageId) {
-		int cnt = service.selectDeliveryGoods(storageId);
->>>>>>> branch 'SONGJEONGEUN' of https://github.com/devA-2/YeolLockMalLock.git
 		System.out.println("배송물량!! "+ cnt);
 		return cnt<7? "success":"false";
 	}
