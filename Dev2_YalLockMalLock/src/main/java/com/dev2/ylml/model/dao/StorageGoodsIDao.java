5c9dd8d05006b85658f7417c32fd94500a79e091
jeongeun15 <58517997+jeongeun15@users.noreply.github.com>
2021-01-10 23:27:57 +0900

    Update StorageGoodsIDao.java

--- Dev2_YalLockMalLock/src/main/java/com/dev2/ylml/model/dao/StorageGoodsIDao.java
+++ Dev2_YalLockMalLock/src/main/java/com/dev2/ylml/model/dao/StorageGoodsIDao.java
@@ -1,7 +1,6 @@
 package com.dev2.ylml.model.dao;
 
 import java.util.List;
-<<<<<<< HEAD
 import java.util.Map;
 
 import com.dev2.ylml.dto.CostDto;
@@ -35,7 +34,6 @@ public interface StorageGoodsIDao {
 	 */
 	public int selectDeliveryQty(String storageId);
 	
-	
 	/**
 	 * 배송 소요 시간 계산
 	 */
@@ -45,30 +43,7 @@ public interface StorageGoodsIDao {
 	 * 배송 비용 계산
 	 */
 	public int selectStationCost(Map<String, String> stations);
-=======
 
-import com.dev2.ylml.dto.CostDto;
-import com.dev2.ylml.dto.UserDeliveryListDto;
-import com.dev2.ylml.dto.UserStorageListDto;
-
-public interface StorageGoodsIDao {
-
-	/**
-	 * 보관 정보 조회(사용자)
-	 */
-	public List<UserStorageListDto> selectUserStorageList(String email);
-	
-	/**
-	 * 보관 비용 조회(사용자)
-	 */
-	public List<CostDto> selectCost(String email);
-	
-	/**
-	 * 배송 물량 확인
-	 */
-	public int selectDeliveryGoods(String storageId);
->>>>>>> branch 'SONGJEONGEUN' of https://github.com/devA-2/YeolLockMalLock.git
-	
 	/**
 	 * 배송 정보 조회(사용자)
 	 */
