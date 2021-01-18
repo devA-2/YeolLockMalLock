d96d664f3923a37a84244b3435895b72ad82e11e
Nerdhead(Ryu_Ju_Seong) <wntjd337@gmail.com>
2021-01-18 13:43:44 +0900

    RJS Import Project : API, USER

--- Api_Test_Project/Ryu_Juseong/Sample_Dev2_YLML_API_Server/src/main/java/com/dev2/ylml/controller/ApiController.java
+++ Api_Test_Project/Ryu_Juseong/Sample_Dev2_YLML_API_Server/src/main/java/com/dev2/ylml/controller/ApiController.java
@@ -3,9 +3,17 @@ package com.dev2.ylml.controller;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -24,6 +32,28 @@ public class ApiController {
 	@RequestMapping(value = "index.do")
 	public List<HashMap<String, Object>> index(){
 		return service.testList();
+	}
+	
+	
+	@ResponseBody
+	@RequestMapping(value = "service.do")
+	public List<Map<String, Object>> service(@RequestBody Map<String, String> data){
+		System.out.println("================================================");
+	    System.out.println(data);
+	    System.out.println("================================================");
+		
+		List<Map<String, Object>> list= new ArrayList<Map<String,Object>>();
+		Map<String, Object> map=new HashMap<String, Object>();
+		map.put("Cert", true);
+		list.add(map);
+
+		map=new HashMap<String, Object>();
+		map.put("id", "wntjd337@gmail.com");
+		map.put("pw", "1234");
+		list.add(map);
+		
+		
+		return list;
 	}
 	
 }
