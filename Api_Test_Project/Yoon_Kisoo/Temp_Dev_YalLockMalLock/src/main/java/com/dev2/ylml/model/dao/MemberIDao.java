b1f33fc855d0e0716368ea1c26dbec8decb1c4f6
KisooYoon <yoooon0104@gmail.com>
2021-01-18 17:42:07 +0900

    YKS_COMMIT - COMMIT

--- Api_Test_Project/Yoon_Kisoo/Temp_Dev_YalLockMalLock/src/main/java/com/dev2/ylml/model/dao/MemberIDao.java
+++ Api_Test_Project/Yoon_Kisoo/Temp_Dev_YalLockMalLock/src/main/java/com/dev2/ylml/model/dao/MemberIDao.java
@@ -8,85 +8,115 @@ import com.dev2.ylml.dto.UserGoodsDto;
 
 public interface MemberIDao {
 	
-	/*
-	 *  회원가입(일반회원)
+	/**
+	 * 회원가입
+	 * @param dto
+	 * @return boolean
 	 */
 	public boolean insertMember(MemberDto dto);
 	
-	/*
-	 *  아이디 중복검사(Ajax)
+	/**
+	 * 아이디 중복검사
+	 * @param email
+	 * @return int
 	 */
 	public int idCheck(String email);
 	
-	/*
-	 *  휴대폰 중복검사(Ajax)
+	/**
+	 * 휴대폰 중복검사
+	 * @param phoneNum
+	 * @return
 	 */
 	public int phoneCheck(String phoneNum);
 	
-	/*
-	 *  로그인
+	/**
+	 * 로그인
+	 * @param map
+	 * @return dto
 	 */
 	public MemberDto login(Map<String, Object> map);
 	
-	/*
-	 * 간편 로그인
+	/**
+	 * api 간편 로그인
+	 * @param map
+	 * @return dto
 	 */
 	public MemberDto apiLogin(Map<String, Object> map);
 	
-	/*
-	 * 이메일 인증 후, 권한변경
+	/**
+	 * 일반회원 임시권한 변경
+	 * @param dto
+	 * @return boolean
 	 */
 	public boolean authUpdate(MemberDto dto);
 	
-	/*
-	 *  아이디 찾기
+	/**
+	 * 아이디 찾기 
+	 * @param map
+	 * @return String
 	 */
 	public String idSearch(Map<String, Object> map);
 	
-	/*
-	 *  비밀번호 찾기
+	/**
+	 * 비밀번호 찾기 (입력한 정보 유효한지 검사)
+	 * @param map
+	 * @return int
 	 */
 	public int pwSearch(Map<String, String> map);
 	
-	/*
-	 *  개인정보 변경(지금은 휴대폰 번호만 변경 가능하지만, 추후에 추가 할 수 있음)
+	/**
+	 * 개인정보 변경(지금은 휴대폰 번호만 변경 가능하지만, 추후에 추가 될 수 있음)
+	 * @param map
+	 * @return
 	 */
 	public int updateInfo(Map<String, Object> map);
 	
-	/*
+	/**
 	 * 비밀번호 변경
+	 * @param dto
+	 * @return int
 	 */
-	
 	public int updatePw(MemberDto dto);
 	
-	/*
-	 * 탈퇴전 서비스 사용유무 조회
+	/**
+	 * 탈퇴전 서비스 사용여부확인
+	 * @param email
+	 * @return int
 	 */
 	public int usingCheck(String email);
 	
-	/*
-	 * 회원탈퇴 (삭제여부 변경)
+	/**
+	 * 회원탈퇴
+	 * @param email
+	 * @return int
 	 */
 	public int quitMember(String email);
 	
-	/*
-	 * 
+	/**
+	 * 회원 전체 목록 조회
+	 * @param email
+	 * @return list
 	 */
 	public List<MemberDto> selectAll(String email);
 	
-	/*
-	 *  회원 아이디로 검색
+	/**
+	 * 회원 아이디로 검색
+	 * @return list
 	 */
 	public List<String> memberIdSearch();
 	
-	/*
-	 * 회원 상세정보 조회
+	/**
+	 * 회원 상세 정보 조회
+	 * @param email
+	 * @return dto
 	 */
 	public MemberDto detailMember(String email);
 	
-	/*
-	 * 
+	/**
+	 * 회원 사용중인 서비스 조회
+	 * @param email
+	 * @return list
 	 */
 	public List<UserGoodsDto> memberUsing(String email);
-	
+
 }
