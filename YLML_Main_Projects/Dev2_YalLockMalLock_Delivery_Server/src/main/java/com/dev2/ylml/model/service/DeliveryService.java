afa49f42e75648e182129589a145812247e5cc18
Nerdhead(Ryu_Ju_Seong) <wntjd337@gmail.com>
2021-01-19 18:48:29 +0900

    RJS Commit : Api Server -> 오류 해결, Delivery 서비스 예제 업데이트

--- YLML_Main_Projects/Dev2_YalLockMalLock_Delivery_Server/src/main/java/com/dev2/ylml/model/service/DeliveryService.java
+++ YLML_Main_Projects/Dev2_YalLockMalLock_Delivery_Server/src/main/java/com/dev2/ylml/model/service/DeliveryService.java
@@ -24,8 +24,15 @@ public class DeliveryService implements DeliveryIService{
 		map.put("id", id);
 		map.put("pw", pw);
 		
+		//API 서버에서 helper.generateData(sendData)안의 sendData 의 타입으로 돌려줌
+		//helper.getData(map)가 캐스팅된 타입이 helper.request("sampleData.do", map)의 map에 들어감
 		return (HashMap<String, String>)helper.request("sampleData.do", map);
 	}
 	
+	//public boolean insertMember(MemberDto memberDto) {
+	//	return (boolean)helper.request("insertMember.do", memberDto);
+	//}
+	//필요한 dto 들고 오셔야하구여
+	//iService 에 본인이 사용할 메소드들을 api 서버에서 복사해서 input(파라미터) output(리턴) 수정해주시고 구현해주시면 됩니당
 	
 }
