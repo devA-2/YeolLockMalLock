09ebc5cb2f5e6cb28c2494b86c84dee9d6eadd0d
AHNJIHOON <jamza2@naver.com>
2021-01-19 16:22:45 +0900

    AJH SERVICE UPDATE : selectDetail 잘못 바꾼거 원상복구

--- YLML_Main_Projects/Dev2_YalLockMalLock_API_Server/src/main/java/com/dev2/ylml/model/service/Api_IService.java
+++ YLML_Main_Projects/Dev2_YalLockMalLock_API_Server/src/main/java/com/dev2/ylml/model/service/Api_IService.java
@@ -352,7 +352,7 @@ public interface Api_IService {
 	 * @param seq
 	 * @return
 	 */
-	public Map<String, Object>/* ReportDto */ selectReportDetail(Map<String, Object> map/* String seq */);
+	public Map<String, Object>/* ReportDto */ selectDetailGoReply(Map<String, Object> map/* String seq */);
 
 	/**
 	 * 신고 글을 email로 검색
@@ -401,7 +401,7 @@ public interface Api_IService {
 	 * @param email
 	 * @return
 	 */
-	public Map<String, Object>/* Manager_MemberDto */ selectDetailGoReply(Map<String, Object> map /* String email */);
+	public Map<String, Object>/* Manager_MemberDto */ selectDetail(Map<String, Object> map /* String email */);
 
 	/**
 	 * 배송 정보조회
