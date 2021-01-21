c8600cdf3653657d953628f07876fa6ae79fbd06
AHNJIHOON <jamza2@naver.com>
2021-01-21 15:05:14 +0900

    AJH UPDATE PROJECT : Controller, dto, service, views 추가

--- YLML_Main_Projects/Dev2_YalLockMalLock_User_Server/src/main/java/com/dev2/ylml/model/service/UserIService.java
+++ YLML_Main_Projects/Dev2_YalLockMalLock_User_Server/src/main/java/com/dev2/ylml/model/service/UserIService.java
@@ -13,6 +13,9 @@ import com.dev2.ylml.dto.StorageBoxDto;
 import com.dev2.ylml.dto.StorageGoodsDto;
 import com.dev2.ylml.dto.StorageListDto;
 import com.dev2.ylml.dto.UserStorageListDto;
+import com.dev2.ylml.dto.LostPropertyDto;
+import com.dev2.ylml.dto.RFIDDto;
+import com.dev2.ylml.dto.ReportDto;
 
 /**
  * @author nerdhead
@@ -262,5 +265,114 @@ public interface UserIService {
 	 * 결제 상태 업데이트
 	 */
 	public boolean updateCostStatus(String costCode);
+	
+	/** 유실물 전체 리스트 조회
+	 * @return
+	 */
+	public List<LostPropertyDto> selectAllLostProperty();
+	
+	/** 유실물 상세 조회
+	 * @param seq
+	 * @return
+	 */
+	public LostPropertyDto selectOneLostProperty(String seq);
+	
+	/** 신고 글 작성
+	 * @param dto
+	 * @return
+	 */
+	public boolean insertReport(ReportDto dto);
+
+	/** 답변 글 작성 페이지 이동
+	 * @param dto
+	 * @return
+	 */
+	public boolean replyReport(ReportDto dto);
+
+	/** 답변 글 작성 실행
+	 * @param dto
+	 * @return
+	 */
+	public boolean reply(ReportDto dto);
+	
+	/** 신고 글 수정 페이지 이동
+	 * @param dto
+	 * @return
+	 */
+	public boolean modifyReport(ReportDto dto);
+
+	/** 전체 신고 글 리스트 조회
+	 * @return
+	 */
+	public List<ReportDto> selectAllReport();
+
+	/** 신고 글 상세 조회
+	 * @param refer
+	 * @return
+	 */
+	public List<ReportDto> selectDetailReport(String refer);
+
+	/** 처리 상태 변경
+	 * @param dto
+	 * @return
+	 */
+	public boolean updateProcessStatus(ReportDto dto);
+	
+	/** 상세 조회한 신고 글로 다시 접근
+	 * @param seq
+	 * @return
+	 */
+	public ReportDto selectDetail(String seq);
+	
+	/** 신고 글을 email로 검색
+	 * @param email
+	 * @return
+	 */
+	public List<ReportDto> searchId(String email); // 신고글 검색
+	
+	/** 유실물 글을 email로 검색
+	 * @param receipt_user_id
+	 * @return
+	 */
+	public List<LostPropertyDto> searchId2(String receipt_user_id); // 유실물 검색
+	
+	/** 유실물 입력
+	 * @param dto
+	 * @return
+	 */
+	public boolean insertLostProperty(ReportDto dto);
+	
+	// ------------------------------- 물품 보관 ----------------------------- 
+	/**
+	 * 키 제외하고 물품부터 등록
+	 * @param dto
+	 * @return
+	 */
+	public boolean insertGoods(RFIDDto dto);
+	
+	/**
+	 * 물품 보관함의 seq, 보관하는 사람의 email, 보관하는 사람의 TAG를 합쳐 키 생성하고 키 등록하기
+	 * @param dto
+	 * @return
+	 */
+	public boolean insertKey(RFIDDto dto);
+	
+// ----------------------------- 키 전송 -----------------------------
+	
+	/**
+	 * STORAGE_GOODS의 OUT_USER를 키의 수신자이자 보관함에서 물품을 꺼내는 사람의 email로 변경
+	 * @param dto
+	 * @return
+	 */
+	public boolean updateOutUser(RFIDDto dto);
+	
+	
+	/**
+	 * STORAGE_GOODS의 KEY를 보관함에서 물품을 꺼내는 사람의 KEY로 재설정해주기
+	 * -> 보관함 seq + 보관함에서 물품 꺼내는 사람의 email + 보관함에서 물품 꺼내는 사람의 tag로 키 생성해서 update.
+	 * @param dto
+	 * @return
+	 */
+	public boolean updateKey(RFIDDto dto);
 	
 }
---
+++ YLML_Main_Projects/Dev2_YalLockMalLock_User_Server/src/main/webapp/WEB-INF/views/board/lostPropertyList.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Insert title here</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+</head>
+<body>
+<div style="width: 360px; height: 600px; border: 1px solid black; margin:auto;">
+	<table class="table table-hover">
+		<tr>
+			<th>연번</th>
+			<th>소유자</th>
+			<th>등록일</th>
+		</tr>
+		<c:forEach items="${lists}" var="vo" varStatus="vs">
+			<tr onclick="location.href='./selectOneLostProperty.do?seq=${vo.seq}'">
+				<td>${vo.seq}</td>
+				<td>${vo.receipt_user_id}</td>
+				<td>${vo.lost_regdate}</td>
+			</tr>
+		</c:forEach>
+	</table>
+	
+	<div>
+		<button onclick="location.href='#'">메인으로</button>
+		<form action="./searchIdLostProperty.do" method="post">
+			<input type="text" value="USER02@NAVER.COM" name="receipt_user_id">
+			<input type="submit" value="검색">
+		</form>
+	</div>
+</div>
+</body>
+</html>
